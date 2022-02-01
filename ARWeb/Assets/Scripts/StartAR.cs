8f5fa5551bed68ea27369f7546e7e53327c2636f
Alvarden <alvaroracchumi@hotmail.com>
2022-02-01 16:52:52 -0500

    Preparation for treating images

--- ARWeb/Assets/Scripts/StartAR.cs
+++ ARWeb/Assets/Scripts/StartAR.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 using UnityEngine.XR;
+using WebXR;
 
 public class StartAR : MonoBehaviour
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
